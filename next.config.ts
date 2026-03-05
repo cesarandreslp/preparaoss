f240dbebd4104825198a5577098215a3122a49cb
cesarandreslopu-6636 <cesarandres.lopu@gmail.com>
2026-03-05 13:56:30 -0500

    fix: remove next-pwa for Vercel compatibility (Turbopack conflict)

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,5 @@
 import type { NextConfig } from "next";
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "google-fonts",
-          expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/.*\.neon\.tech\/.*/i,
-        handler: "NetworkOnly",
-      },
-    ],
-  },
-});
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -33,4 +9,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
